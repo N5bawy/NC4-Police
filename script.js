07843e26c332111d78f8a41b6ea929d600ea664b
N5bawy <ywzra99@gmail.com>
2026-05-08 01:53:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 console.log("NC4 SYSTEM LOADED")
 
-document.addEventListener("DOMContentLoaded", () => {
+const cards = document.querySelectorAll(".card")
 
-  console.log("NC4 READY")
+cards.forEach(card => {
+
+  card.addEventListener("mouseenter", () => {
+
+    card.style.transform = "translateY(-5px)"
+    card.style.transition = ".3s"
+
+  })
+
+  card.addEventListener("mouseleave", () => {
+
+    card.style.transform = "translateY(0px)"
+
+  })
 
 })
+
+const announcements = document.querySelectorAll(".announcement")
+
+announcements.forEach(item => {
+
+  item.addEventListener("mouseenter", () => {
+
+    item.style.border = "1px solid #2196f3"
+
+  })
+
+  item.addEventListener("mouseleave", () => {
+
+    item.style.border = "1px solid transparent"
+
+  })
+
+})
+
+console.log("NC4 READY")
